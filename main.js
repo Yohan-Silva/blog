7a529a9e0368b57fb0b2739e463d2d1086d6caa5
Yohan-Silva <115710078+Yohan-Silva@users.noreply.github.com>
2023-06-15 08:06:24 -0300

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-const ativaMenu = document.querySelector('.fa-bars')
-const navMenu = document.querySelector('header .menu')
-
-ativaMenu.addEventListener('click', () => {
-    ativaMenu.classList.toggle('fa-x')
-    navMenu.classList.toggle('ativado')
-})
